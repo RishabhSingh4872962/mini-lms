fc2e17cf8d09ad5132206b228130cbc814b54e2b
Rishabh Singh <rs4872962@gmail.com>
2026-03-30 09:22:20 +0530

    add redirect to tabs if user authenticated

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,7 @@
-import { Text, View } from "react-native";
+import { useAuthStore } from "@/stores/authStore";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl items-center text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
+  const { isAuthenticated } = useAuthStore();
+  return <Redirect href={isAuthenticated ? "/(tabs)" : "/(auth)/login"} />;
 }
