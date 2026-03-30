e782ec25f959d863a3b50a057d380cedf0e7c1f6
Rishabh Singh <rs4872962@gmail.com>
2026-03-30 10:16:03 +0530

    upadte app layout screen for offline and add network context provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
+import { ErrorBoundary } from "@/components/common/ErrorBoundary";
+import { NetworkStatusProvider } from "@/components/common/NetworkStatusProvider";
+import { OfflineBanner } from "@/components/common/OfflineBanner";
 import { useAuthStore } from "@/stores/authStore";
-import { SplashScreen, Stack } from "expo-router";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
-import "../global.css"; // NativeWind
+import "../global.css";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -21,18 +25,32 @@ export default function RootLayout() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen
-            name="course/[id]"
-            options={{
-              animation: "slide_from_right",
-              presentation: "card",
-            }}
-          />
-        </Stack>
+        <ErrorBoundary>
+          <NetworkStatusProvider>
+            {/* Floating offline banner — sits above everything */}
+            <OfflineBanner />
+
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" />
+              <Stack.Screen name="(auth)" />
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen
+                name="course/[id]"
+                options={{
+                  animation: "slide_from_right",
+                  presentation: "card",
+                }}
+              />
+              <Stack.Screen
+                name="course/webview/[id]"
+                options={{
+                  animation: "slide_from_bottom",
+                  presentation: "card",
+                }}
+              />
+            </Stack>
+          </NetworkStatusProvider>
+        </ErrorBoundary>
       </GestureHandlerRootView>
     </SafeAreaView>
   );
