ce5ac78bcacbc7a5ef9bfa2b9fa8c06c33cfb1bc
Rishabh Singh <rs4872962@gmail.com>
2026-03-30 09:16:21 +0530

    setup auth router and index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,31 @@
-import { Stack } from "expo-router";
-import "../global.css";
+import { useAuthStore } from "@/stores/authStore";
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css"; // NativeWind
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  return <Stack />;
+  const { checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth().finally(async () => {
+      await SplashScreen.hideAsync();
+    });
+  }, []);
+
+  if (isCheckingAuth) return null;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </GestureHandlerRootView>
+    </SafeAreaView>
+  );
 }
